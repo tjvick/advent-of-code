e55c47a1714164804e979ce9a0f76c3aee9c639b
Tyler Vick <vick.tyler@gmail.com>
2019-12-01 00:37:05 -0500

    day 1 complete

--- 2019_python/solutions/day1/part1.py
+++ 2019_python/solutions/day1/part1.py
@@ -1,2 +1,10 @@
+import math
+sum = 0
+
 with open('input.txt', 'r') as f:
     for line in f:
+        mass = int(line.strip('\n'))
+        fuel = math.floor(mass / 3) - 2
+        sum += fuel
+
+print(sum)
--- 2019_python/solutions/day1/part2.py
+++ 2019_python/solutions/day1/part2.py
@@ -1,5 +1,15 @@
-# with open('input.txt', 'r') as f:
-#     wholefile = f.read()
-#
-# with open('input.txt', 'r') as f:
-#     for line in f:
+import math
+sum = 0
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        mass = int(line.strip('\n'))
+        added_fuel = math.floor(mass / 3) - 2
+        module_sum = added_fuel
+        while added_fuel > 0:
+            added_fuel = max(math.floor(added_fuel / 3) - 2, 0)
+            module_sum += added_fuel
+
+        sum += module_sum
+
+print(sum)
