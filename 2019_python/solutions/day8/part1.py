e1d31f4c102a69f914c1c052fcc09142387ee67d
Tyler Vick <vick.tyler@gmail.com>
2019-12-08 00:59:51 -0500

    refactor 8.1

--- 2019_python/solutions/day8/part1.py
+++ 2019_python/solutions/day8/part1.py
@@ -1,23 +1,23 @@
 from collections import Counter
 import numpy as np
+from functools import reduce
 
 with open('input.txt', 'r') as f:
     for line in f:
         content = line.strip('\n')
 
-x = np.array(list(content))
-a = x.reshape([100, 6, 25])
+a = np.array(list(content)).reshape([100, 6, 25])
 
-max_zeros = 10000000
-for il in range(100):
-    count = 0
-    b = (a[il][:][:]).flatten()
-    c = Counter(b)
-    print(len(b))
-    n_zeros = c["0"]
-    if n_zeros < max_zeros:
-        max_zeros = n_zeros
-        answer = c["1"]*c["2"]
 
+def reduce_fn(acc, layer):
+    c = Counter(layer.flatten())
+    if c['0'] < acc[0]:
+        return c['0'], c['1']*c['2']
+    return acc
+
+
+result = reduce(reduce_fn, (layer for layer in a), (np.inf, 0))
+
+print(result)
 
 
