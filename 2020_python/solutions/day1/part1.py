b4b501442777cf2b5db9194375575faea5995cc6
Tyler Vick <vick.tyler@gmail.com>
2020-12-01 00:10:27 -0500

    Day 1 2020

--- 2020_python/solutions/day1/part1.py
+++ 2020_python/solutions/day1/part1.py
@@ -1,3 +1,13 @@
+values = []
 with open('input.txt', 'r') as f:
     for line in f:
-        content = line.strip('\n')
+        value = int(line.strip('\n'))
+        values.append(value)
+
+values.sort()
+print(values)
+
+for value1 in values:
+    for value2 in values:
+        if (value1 + value2) == 2020:
+            print(value1 * value2)
--- 2020_python/solutions/day1/part2.py
+++ 2020_python/solutions/day1/part2.py
@@ -1,3 +1,11 @@
+values = []
 with open('input.txt', 'r') as f:
     for line in f:
-        content = line.strip('\n')
+        value = int(line.strip('\n'))
+        values.append(value)
+
+for value1 in values:
+    for value2 in values:
+        for value3 in values:
+            if (value1 + value2 + value3) == 2020:
+                print(value1 * value2 * value3)
