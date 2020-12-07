2ab1b8ed08bf149beb1bee34021850e84258806e
Tyler Vick <vick.tyler@gmail.com>
2020-12-07 13:49:16 -0500

    Day 7 - refactor the refactoring

--- 2020_python/solutions/day7/part1_better.py
+++ 2020_python/solutions/day7/part1_better.py
@@ -1,29 +1,38 @@
 import re
 
+
+def build_tree_edges(file_contents):
+    edges = []
+    rule_re = re.compile(r'^([\w\s]+) bags contain (\d+ .* bags?[,.])+$')
+    for rule in file_contents:
+        m = rule_re.match(rule)
+        if m:
+            outer_color = m.group(1)
+            inner_colors = re.findall(r'\d+ ([\w\s]+) bags?', m.group(2))
+
+            for inner_color in inner_colors:
+                edges.append((outer_color, inner_color))
+
+    return edges
+
+
+def travel_up_tree(unweighted_edges, starting_node):
+    target_nodes = [starting_node]
+    visited_nodes = []
+    while len(target_nodes) > 0:
+        node = target_nodes.pop()
+        visited_nodes.append(node)
+        target_nodes += [source_node for source_node, target_node in unweighted_edges if target_node == node]
+
+    return visited_nodes
+
+
 with open('input.txt', 'r') as f:
     file_contents = [line.strip('\n') for line in f]
 
-edges = []
-rule_re = re.compile(r'^([\w\s]+) bags contain (\d+ .* bags?[,.])+$')
-for rule in file_contents:
-    m = rule_re.match(rule)
-    if m:
-        outer_color = m.group(1)
-        inner_colors = re.findall(r'\d+ ([\w\s]+) bags?', m.group(2))
-
-        for inner_color in inner_colors:
-            edges.append((outer_color, inner_color))
-
-
-nodes_to_check = ['shiny gold']
-all_visited_nodes = []
-while len(nodes_to_check) > 0:
-    node = nodes_to_check.pop()
-    all_visited_nodes.append(node)
-    containing_nodes = [outer_node for outer_node, inner_node in edges if inner_node == node]
-    nodes_to_check += containing_nodes
+edges = build_tree_edges(file_contents)
+all_visited_nodes = travel_up_tree(edges, 'shiny gold')
 
 unique_containing_nodes = set(all_visited_nodes)
 unique_containing_nodes.remove('shiny gold')
 print(len(unique_containing_nodes))
-
--- 2020_python/solutions/day7/part2_better.py
+++ 2020_python/solutions/day7/part2_better.py
@@ -1,34 +1,39 @@
 import re
 
+
+def build_tree_edges(file_contents):
+    edges = []
+    rule_re = re.compile(r'^([\w\s]+) bags contain (\d+ .*)$')
+    for rule in file_contents:
+        rule_match = rule_re.match(rule)
+        if rule_match:
+            outer_color = rule_match.group(1)
+            inner_colors = re.findall(r'(\d+) ([\w\s]+) bags?', rule_match.group(2))
+            for (count, inner_color) in inner_colors:
+                edges.append((outer_color, inner_color, int(count)))
+
+    return edges
+
+
+def travel_down_tree(weighted_edges, starting_node, starting_weight):
+    source_nodes = [(starting_node, starting_weight)]
+    visited_nodes = []
+    while len(source_nodes) > 0:
+        source_node, weight = source_nodes.pop()
+        visited_nodes.append((source_node, weight))
+        attached_edges = [edge for edge in weighted_edges if edge[0] == source_node]
+        source_nodes += [(target_node, count*weight) for _, target_node, count in attached_edges]
+
+    return visited_nodes
+
+
 with open('input.txt', 'r') as f:
     file_contents = [line.strip('\n') for line in f]
 
-edges = []
-rule_re = re.compile(r'^([\w\s]+) bags contain (\d+ .*)$')
-empty_rule_re = re.compile(r'^([\w\s]+) bags contain no other bags.$')
-for rule in file_contents:
-    rule_match = rule_re.match(rule)
-    if rule_match:
-        outer_color = rule_match.group(1)
-        inner_colors = re.findall(r'(\d+) ([\w\s]+) bags?', rule_match.group(2))
-        for inner_color in inner_colors:
-            edges.append((outer_color, inner_color[1], int(inner_color[0])))
-    else:
-        empty_rule_match = empty_rule_re.match(rule)
-        outer_color = empty_rule_match.group(1)
+edges = build_tree_edges(file_contents)
+all_visited_nodes = travel_down_tree(edges, 'shiny gold', 1)
 
-
-source_nodes = [('shiny gold', 1)]
-
-all_node_visits = []
-while len(source_nodes) > 0:
-    source_node, weight = source_nodes.pop()
-    all_node_visits.append((source_node, weight))
-    attached_edges = [edge for edge in edges if edge[0] == source_node]
-    for edge in attached_edges:
-        source_nodes.append((edge[1], edge[2] * weight))
-
-sum_of_all_bags = sum([count for _, count in all_node_visits])
+sum_of_all_bags = sum([count for _, count in all_visited_nodes])
 print(sum_of_all_bags - 1)
 
 
