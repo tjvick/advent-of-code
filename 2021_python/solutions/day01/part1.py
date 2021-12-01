eb7058eec933ba53e90de79a022913600860a244
Tyler Vick <vick.tyler@gmail.com>
2021-12-01 00:08:49 -0500

    improve day 1 part 1 solution a bit

--- 2021_python/solutions/day01/part1.py
+++ 2021_python/solutions/day01/part1.py
@@ -6,11 +6,9 @@ values = helpers.read_as_int_list(filename)
 print(values)
 
 count = 0
-prev_value = 100000000
-for value in values:
-    if value > prev_value:
+for ix, value in enumerate(values[1:]):
+    if value > values[ix]:
         count += 1
-    prev_value = value
 
 print(count)
 
