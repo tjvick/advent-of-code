56b16ae07b420e0c7bf05d19d9d8a1e72e524654
Tyler Vick <vick.tyler@gmail.com>
2021-12-01 21:57:13 -0500

    day 2, 2017 improvement

--- 2017_python/solutions/day02/part1.py
+++ 2017_python/solutions/day02/part1.py
@@ -4,8 +4,6 @@ filename = 'input'
 
 integer_lists = helpers.read_as_delimited_integer_lists(filename)
 
-print(integer_lists)
-
 checksum = 0
 for integer_list in integer_lists:
     checksum += max(integer_list) - min(integer_list)
--- 2017_python/solutions/day02/part2.py
+++ 2017_python/solutions/day02/part2.py
@@ -5,17 +5,16 @@ filename = 'input'
 
 integer_lists = helpers.read_as_delimited_integer_lists(filename)
 
-checksum = 0
-for integer_list in integer_lists:
-    sorted = np.sort(integer_list)
-    factor = 0
-    for ixs, smaller in enumerate(sorted):
-        for larger in sorted[ixs+1:]:
-            if larger % smaller == 0:
-                factor = larger / smaller
-                break
 
-    checksum += factor
+def find_factor(integer_list):
+    sorted_ints = np.sort(integer_list)
+    for ixs, smaller in enumerate(sorted_ints):
+        for larger in sorted_ints[ixs + 1:]:
+            if larger % smaller == 0:
+                return larger / smaller
+
+
+checksum = sum(find_factor(integer_list) for integer_list in integer_lists)
 
 print(checksum)
 
