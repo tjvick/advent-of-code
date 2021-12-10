ad88f3d6d03b18e538a910cfbe59d7ad030d83dc
Tyler Vick <vick.tyler@gmail.com>
2021-12-10 00:24:32 -0500

    2021.10 slightly cleaner

--- 2021_python/solutions/day10/part1.py
+++ 2021_python/solutions/day10/part1.py
@@ -1,38 +1,26 @@
 from solutions import helpers
-import numpy as np
-import re
 
 filename = 'input'
 
-# strings = helpers.read_each_line_as_string(filename)
-# ints = helpers.read_each_line_as_int(filename)
-# floats = helpers.read_each_line_as_float(filename)
 char_sequences = helpers.read_each_line_as_char_sequence(filename)
-# digit_sequences = helpers.read_each_line_as_digit_sequence(filename)
-# int_sequences = helpers.read_each_line_as_delimited_int_sequence(filename)
 
-openers = ['(', '[', '{', '<']
-closers = [')', ']', '}', '>']
-the_openers = {
+openers_by_closer = {
     ')': '(',
     ']': '[',
     '}': '{',
     '>': '<',
 }
+openers = openers_by_closer.values()
 
 illegals = []
-print(char_sequences)
 for sequence in char_sequences:
-    print(sequence)
     stack = []
     for char in sequence:
         if char in openers:
             stack.append(char)
-        elif stack.pop() == the_openers[char]:
-            # print('good')
+        elif stack.pop() == openers_by_closer[char]:
             pass
         else:
-            print('bad')
             illegals.append(char)
 
 
@@ -42,6 +30,7 @@ points = {
     '}': 1197,
     '>': 25137,
 }
+
 answer = sum(points[char] for char in illegals)
 print(answer)
 
--- 2021_python/solutions/day10/part2.py
+++ 2021_python/solutions/day10/part2.py
@@ -1,49 +1,35 @@
 from solutions import helpers
 import numpy as np
-import re
 
 filename = 'input'
 
-# strings = helpers.read_each_line_as_string(filename)
-# ints = helpers.read_each_line_as_int(filename)
-# floats = helpers.read_each_line_as_float(filename)
 char_sequences = helpers.read_each_line_as_char_sequence(filename)
-# digit_sequences = helpers.read_each_line_as_digit_sequence(filename)
-# int_sequences = helpers.read_each_line_as_delimited_int_sequence(filename)
 
-openers = ['(', '[', '{', '<']
-closers = [')', ']', '}', '>']
-the_openers = {
+openers_by_closer = {
     ')': '(',
     ']': '[',
     '}': '{',
     '>': '<',
 }
+openers = openers_by_closer.values()
 
-the_closers = {
-    '(': ')',
-    '[': ']',
-    '{': '}',
-    '<': '>',
-}
+closers_by_opener = {v: k for k, v in openers_by_closer.items()}
 
-points = {
+points_by_closer = {
     ')': 1,
     ']': 2,
     '}': 3,
     '>': 4,
 }
 
-illegals = []
 total_scores = []
-print(char_sequences)
 for sequence in char_sequences:
     stack = []
     bad = False
     for char in sequence:
         if char in openers:
             stack.append(char)
-        elif stack.pop() == the_openers[char]:
+        elif stack.pop() == openers_by_closer[char]:
             pass
         else:
             bad = True
@@ -51,14 +37,8 @@ for sequence in char_sequences:
     if not bad and len(stack) > 0:
         total_score = 0
         for char in reversed(stack):
-            total_score = total_score*5 + points[the_closers[char]]
-        print(total_score)
+            total_score = total_score * 5 + points_by_closer[closers_by_opener[char]]
         total_scores.append(total_score)
 
-
-answer = sum(points[char] for char in illegals)
-print(answer)
-
 print(np.median(total_scores))
-
 
