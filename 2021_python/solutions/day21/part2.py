72a6032c4e9df7846647cf78eef6ac6856ecfbf7
Tyler Vick <vick.tyler@gmail.com>
2021-12-21 23:36:03 -0500

    2021.21 minor change

--- 2021_python/solutions/day21/part2.py
+++ 2021_python/solutions/day21/part2.py
@@ -1,20 +1,10 @@
-from solutions import helpers
+from functools import cache
 import numpy as np
-import re
 
 np.set_printoptions(edgeitems=30, linewidth=100000)
 
-# filename = 'input'
-
-# strings = helpers.read_each_line_as_string(filename)
-# ints = helpers.read_each_line_as_int(filename)
-# floats = helpers.read_each_line_as_float(filename)
-# char_sequences = helpers.read_each_line_as_char_sequence(filename)
-# digit_sequences = helpers.read_each_line_as_digit_sequence(filename)
-# int_sequences = helpers.read_each_line_as_delimited_int_sequence(filename)
-
-player_1_position = 4
-player_2_position = 8
+# player_1_position = 4
+# player_2_position = 8
 player_1_position = 9
 player_2_position = 3
 player_1_score = 0
@@ -42,46 +32,52 @@ freq_by_sum = {
     9: 1
 }
 
+freq_by_sum_items = freq_by_sum.items()
 
-def split_universes(position_1, score_1, position_2, score_2, turn, n_universes):
+
+@cache
+def move(starting_position, dice_total):
+    return (starting_position + dice_total - 1) % 10 + 1
+
+
+def split_universes(position_1, score_1, position_2, score_2, turn):
     if turn == 1:
         if score_1 == 20:
-            return n_universes * 27, 0
+            return 27, 0
 
         p1_wins_sum = 0
         p2_wins_sum = 0
-        for val, freq in freq_by_sum.items():
+        for val, freq in freq_by_sum_items:
             dice_total = val
-            player_1_position = (position_1 + dice_total - 1) % 10 + 1
+            player_1_position = move(position_1, dice_total)
             player_1_score = score_1 + player_1_position
             if player_1_score >= 21:
                 p1_wins_sum += freq
             else:
-                p1_wins, p2_wins = split_universes(player_1_position, player_1_score, position_2, score_2, 2, freq)
-                p1_wins_sum += p1_wins
-                p2_wins_sum += p2_wins
+                p1_wins, p2_wins = split_universes(player_1_position, player_1_score, position_2, score_2, 2)
+                p1_wins_sum += freq*p1_wins
+                p2_wins_sum += freq*p2_wins
 
-        return n_universes * p1_wins_sum, n_universes * p2_wins_sum
+        return p1_wins_sum, p2_wins_sum
 
-    if turn == 2:
+    else:
         if score_2 == 20:
-            return 0, n_universes * 27
+            return 0, 27
 
         p1_wins_sum = 0
         p2_wins_sum = 0
-        for val, freq in freq_by_sum.items():
+        for val, freq in freq_by_sum_items:
             dice_total = val
-            player_2_position = (position_2 + dice_total - 1) % 10 + 1
+            player_2_position = move(position_2, dice_total)
             player_2_score = score_2 + player_2_position
             if player_2_score >= 21:
                 p2_wins_sum += freq
             else:
-                p1_wins, p2_wins = split_universes(position_1, score_1, player_2_position, player_2_score, 1, freq)
-                p1_wins_sum += p1_wins
-                p2_wins_sum += p2_wins
-        return n_universes*p1_wins_sum, n_universes*p2_wins_sum
+                p1_wins, p2_wins = split_universes(position_1, score_1, player_2_position, player_2_score, 1)
+                p1_wins_sum += freq * p1_wins
+                p2_wins_sum += freq * p2_wins
+
+        return p1_wins_sum, p2_wins_sum
 
 
-print(split_universes(player_1_position, 0, player_2_position, 0, 1, 1))
-
-# (148747830493442, 89305072914203)
+print(split_universes(player_1_position, 0, player_2_position, 0, 1))
