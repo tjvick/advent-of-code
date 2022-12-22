b2b8980c52f1f42a9e52e866d3284a522473ba7e
Tyler Vick <vick.tyler@gmail.com>
2022-12-21 22:10:03 -0500

    2022 Day 16

--- 2022_python/solutions/day16/part1.py
+++ 2022_python/solutions/day16/part1.py
@@ -1,8 +1,11 @@
+import dataclasses
+import functools
+
 from solutions import helpers
 import numpy as np
 import re
 import networkx as nx
-from itertools import permutations
+from functools import cache
 import matplotlib.pyplot as plt
 
 np.set_printoptions(edgeitems=30, linewidth=100000)
@@ -10,94 +13,114 @@ np.set_printoptions(edgeitems=30, linewidth=100000)
 filename = 'input'
 # filename = 'test'
 
-n_minutes = 30
-
-strings = helpers.read_each_line_as_string(filename)
-
-nodes = []
-edges = []
-flow_rates = {}
-
-for string in strings:
-    m = re.match(r'^Valve (.{2}) has flow rate=(\d+); tunnels? leads? to valves? (.+)$', string)
-    valve_id, flow_rate_str, other_valves_str = m.groups()
-    flow_rate = int(flow_rate_str)
-    connected_valve_ids = other_valves_str.replace(' ', '').split(',')
-
-    print(valve_id, flow_rate, connected_valve_ids)
-
-    flow_rates[valve_id] = flow_rate
-    nodes.append(valve_id)
-    for connected_valve_id in connected_valve_ids:
-        edges.append((valve_id, connected_valve_id))
-
-print(nodes)
-print(edges)
-
-G = nx.Graph()
-for node in nodes:
-    G.add_node(node)
-
-for edge in edges:
-    G.add_edge(*edge)
-
-print(G)
-
-lengths = dict(nx.all_pairs_shortest_path_length(G))
-
-print(lengths)
-
-valves_worth_visiting = [valve_id for valve_id, flow_rate in flow_rates.items() if flow_rate > 0]
-
-print(valves_worth_visiting)
+N_MINUTES = 30
 
 
-def length(pth):
-    total_distance = 0
-    for ix, valve in enumerate(pth[:-1]):
-        distance = nx.shortest_path_length(G, pth[ix], pth[ix + 1])
-        total_distance += (distance + 1)
+def extract_graph_and_flow_rates():
+    strings = helpers.read_each_line_as_string(filename)
 
-    return total_distance
+    nodes = []
+    edges = []
+    flow_rates = {}
+    for string in strings:
+        m = re.match(r'^Valve (.{2}) has flow rate=(\d+); tunnels? leads? to valves? (.+)$', string)
+        valve_id, flow_rate_str, other_valves_str = m.groups()
+        flow_rate = int(flow_rate_str)
+        connected_valve_ids = other_valves_str.replace(' ', '').split(',')
 
+        flow_rates[valve_id] = flow_rate
+        nodes.append(valve_id)
+        for connected_valve_id in connected_valve_ids:
+            edges.append((valve_id, connected_valve_id))
 
-def compute_total_flow(pth):
-    total_distance = 0
-    total_flow = 0
-    current_flow = 0
-    for ix, valve in enumerate(pth[:-1]):
-        distance = nx.shortest_path_length(G, pth[ix], pth[ix + 1])
-        total_flow += current_flow * (distance + 1)
-        current_flow += flow_rates[pth[ix+1]]
-        total_distance += (distance + 1)
+    G = nx.Graph()
+    for node in nodes:
+        G.add_node(node)
 
-    total_flow += (n_minutes - total_distance)*current_flow
+    for edge in edges:
+        G.add_edge(*edge)
 
-    return total_flow
-
-
-# print(compute_total_flow(['AA', 'DD', 'BB', 'JJ', 'HH', 'EE', 'CC']))
-
-
-max_total_flow = 0
-current_valve = 'AA'
-paths = [['AA']]
-while len(paths):
-    path = paths.pop()
-    total_flow = compute_total_flow(path)
-    if total_flow > max_total_flow:
-        max_total_flow = total_flow
-        print('NEW MAX!', max_total_flow, path)
-    unvisited_valves = [v for v in valves_worth_visiting if v not in path]
-    for unvisited_valve in unvisited_valves:
-        possible_path = path + [unvisited_valve]
-        # print("Length", length(possible_path), "Possible Path", possible_path)
-        if length(possible_path) <= n_minutes:
-            paths.append(possible_path)
-
-print(len(paths))
-
-# print(length(['AA', 'DD', 'BB', 'JJ', 'HH', 'EE', 'CC']))
-# print(compute_total_flow(['AA', 'DD', 'BB', 'JJ', 'HH', 'EE', 'CC']))
+    return G, flow_rates
 
 
+def get_valves_worth_visiting(flow_rates):
+    return [valve_id for valve_id, flow_rate in flow_rates.items() if flow_rate > 0]
+
+
+G, flow_rates = extract_graph_and_flow_rates()
+valves_worth_visiting = get_valves_worth_visiting(flow_rates)
+set_of_valves_worth_visiting = set(valves_worth_visiting)
+
+
+def show_graph(graph):
+    plt.figure(figsize=(10, 10))
+    nx.draw(
+        graph,
+        nodelist=valves_worth_visiting + ["AA"],
+        labels={node: f"{node}/{flow_rate}" for node, flow_rate in flow_rates.items()},
+        font_weight='normal'
+    )
+    plt.show()
+
+
+# show_graph(G)
+
+
+@cache
+def distance_between(valve_1, valve_2):
+    return nx.shortest_path_length(G, valve_1, valve_2)
+
+
+@dataclasses.dataclass(frozen=True)
+class ValvePath:
+    path: tuple
+    length: int
+    total_flow: int
+    n_minutes: int
+
+    @property
+    def last_valve(self):
+        return self.path[-1]
+
+    def unvisited_valves(self, valves=None):
+        if valves is None:
+            return set_of_valves_worth_visiting.difference(self.path)
+        else:
+            return set(valves).difference(self.path)
+
+    def compute_new_path_length(self, new_valve):
+        return self.length + distance_between(self.last_valve, new_valve) + 1
+
+    def compute_new_valve_path(self, new_valve):
+        new_path = self.path + (new_valve,)
+        new_length = self.compute_new_path_length(new_valve)
+        new_total_flow = self.total_flow + self.compute_delta_total_flow(new_valve)
+        return ValvePath(path=new_path, length=new_length, total_flow=new_total_flow, n_minutes=self.n_minutes)
+
+    def compute_delta_total_flow(self, new_valve):
+        distance = distance_between(self.last_valve, new_valve)
+        time_to_go_turn_on_valve = distance + 1
+        total_time_valve_is_on = self.n_minutes - self.length - time_to_go_turn_on_valve
+        return total_time_valve_is_on * flow_rates[new_valve]
+
+
+def find_best_total_flow(valves=None, n_minutes=30):
+    max_total_flow = 0
+    paths = [ValvePath(path=('AA',), length=0, total_flow=0, n_minutes=n_minutes)]
+    while len(paths):
+        valve_path = paths.pop()
+        if valve_path.total_flow > max_total_flow:
+            max_total_flow = valve_path.total_flow
+
+        for unvisited_valve in valve_path.unvisited_valves(valves):
+            new_valve_path_length = valve_path.compute_new_path_length(unvisited_valve)
+            if new_valve_path_length <= n_minutes:
+                new_valve_path = valve_path.compute_new_valve_path(unvisited_valve)
+                paths.append(new_valve_path)
+
+    return max_total_flow
+
+
+if __name__ == '__main__':
+    max_total_flow = find_best_total_flow(n_minutes=N_MINUTES)
+    print(max_total_flow)
--- 2022_python/solutions/day16/part2.py
+++ 2022_python/solutions/day16/part2.py
@@ -1,107 +1,57 @@
 from solutions import helpers
 import numpy as np
-import re
-import networkx as nx
+from itertools import combinations
+from part1 import extract_graph_and_flow_rates, get_valves_worth_visiting, distance_between, find_best_total_flow
 
 np.set_printoptions(edgeitems=30, linewidth=100000)
 
 filename = 'input'
 # filename = 'test'
 
-n_minutes = 26
+N_MINUTES = 26
 
 strings = helpers.read_each_line_as_string(filename)
 
-nodes = []
-edges = []
-flow_rates = {}
-
-for string in strings:
-    m = re.match(r'^Valve (.{2}) has flow rate=(\d+); tunnels? leads? to valves? (.+)$', string)
-    valve_id, flow_rate_str, other_valves_str = m.groups()
-    flow_rate = int(flow_rate_str)
-    connected_valve_ids = other_valves_str.replace(' ', '').split(',')
-
-    print(valve_id, flow_rate, connected_valve_ids)
-
-    flow_rates[valve_id] = flow_rate
-    nodes.append(valve_id)
-    for connected_valve_id in connected_valve_ids:
-        edges.append((valve_id, connected_valve_id))
-
-print(nodes)
-print(edges)
-
-G = nx.Graph()
-for node in nodes:
-    G.add_node(node)
-
-for edge in edges:
-    G.add_edge(*edge)
-
-print(G)
-
-lengths = dict(nx.all_pairs_shortest_path_length(G))
-
-print(lengths)
-
-valves_worth_visiting = [valve_id for valve_id, flow_rate in flow_rates.items() if flow_rate > 0]
-
-print(valves_worth_visiting)
+G, flow_rates = extract_graph_and_flow_rates()
+valves_worth_visiting = get_valves_worth_visiting(flow_rates)
+set_of_valves_worth_visiting = set(valves_worth_visiting)
 
 
-def length(pth):
-    total_distance = 0
-    for ix, valve in enumerate(pth[:-1]):
-        distance = nx.shortest_path_length(G, pth[ix], pth[ix + 1])
-        total_distance += (distance + 1)
+def find_farthest_pair():
+    farthest_distance = 0
+    farthest_pair = ('AA', 'AA')
+    for pair in combinations(valves_worth_visiting, 2):
+        distance = distance_between(pair[0], pair[1])
+        if distance > farthest_distance:
+            farthest_distance = distance
+            farthest_pair = pair
 
-    return total_distance
-
-
-def compute_total_flow(pth):
-    total_distance = 0
-    total_flow = 0
-    current_flow = 0
-    for ix, valve in enumerate(pth[:-1]):
-        distance = nx.shortest_path_length(G, pth[ix], pth[ix + 1])
-        total_flow += current_flow * (distance + 1)
-        current_flow += flow_rates[pth[ix+1]]
-        total_distance += (distance + 1)
-
-    total_flow += (n_minutes - total_distance)*current_flow
-
-    return total_flow
+    return farthest_pair
 
 
-def compute_combined_flow(path1, path2):
-    return compute_total_flow(path1) + compute_total_flow(path2)
+def find_best_combined_flow(path1_valves, path2_valves, min_split):
+    unassigned_valves = set_of_valves_worth_visiting.difference(path1_valves).difference(path2_valves)
+
+    best_combined_flow = 0
+    for ix in range(min_split, len(unassigned_valves)-min_split):
+        for combo in combinations(unassigned_valves, ix):
+            additional_path1_valves = set(combo)
+            additional_path2_valves = unassigned_valves.difference(additional_path1_valves)
+            best_flow_path1 = find_best_total_flow(valves=additional_path1_valves.union(path1_valves), n_minutes=N_MINUTES)
+            best_flow_path2 = find_best_total_flow(valves=additional_path2_valves.union(path2_valves), n_minutes=N_MINUTES)
+            total_combined_flow = best_flow_path1 + best_flow_path2
+            if total_combined_flow > best_combined_flow:
+                best_combined_flow = total_combined_flow
+                print(best_combined_flow)
+
+    return best_combined_flow
 
 
-# print(compute_total_flow(['AA', 'DD', 'BB', 'JJ', 'HH', 'EE', 'CC']))
+farthest_pair = find_farthest_pair()
+print('Most Distant Pair', farthest_pair)
+best_combined_flow = find_best_combined_flow([farthest_pair[0]], [farthest_pair[1]], 6)
+print('Best Combined Flow:', best_combined_flow)
 
 
-max_total_flow = 0
-current_valve = 'AA'
-path_pairs = [[['AA'], ['AA']]]
-while len(path_pairs):
-    path_pair = path_pairs.pop()
-    total_flow = compute_combined_flow(*path_pair)
-    if total_flow > max_total_flow:
-        max_total_flow = total_flow
-        print('NEW MAX!', max_total_flow, path_pair)
-    unvisited_valves = [v for v in valves_worth_visiting if v not in path_pair[0] + path_pair[1]]
-    path1, path2 = path_pair
-    for unvisited_valve in unvisited_valves:
-        possible_path1 = path1 + [unvisited_valve]
-        if length(possible_path1) <= n_minutes:
-            path_pairs.append([possible_path1, path2])
-    for unvisited_valve in unvisited_valves:
-        possible_path2 = path2 + [unvisited_valve]
-        if length(possible_path2) <= n_minutes:
-            path_pairs.append([path1, possible_path2])
-
-# print(length(['AA', 'DD', 'BB', 'JJ', 'HH', 'EE', 'CC']))
-# print(compute_total_flow(['AA', 'DD', 'BB', 'JJ', 'HH', 'EE', 'CC']))
 
 
